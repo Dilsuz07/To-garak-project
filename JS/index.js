d29d3be0e96b3e02c9ee4057b0503d707d409d17
Dilso`z <dilsuz25012303@gmail.com>
2022-09-25 17:57:33 +0500

    Check success and error Modal Create

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+let success = document.getElementById('success')
+let error = document.getElementById('error')
+function Success() {
+    success.style.display = 'block'
+}
+function Error() {
+    error.style.display = 'block'
+}
